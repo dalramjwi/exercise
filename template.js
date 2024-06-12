9065c78fb11560f826e007be8902042f3b2f15b8
dalramjwi <dalramjwi@gmail.com>
2024-06-12 12:28:46 +0900

    시간과 분

--- template.js
+++ template.js
@@ -1,38 +1,38 @@
 let time = new Date();
 let hours = time.getHours();
 let minutes = time.getMinutes();
+let seconds = time.getSeconds();
 
 const tagComponent = (tagName, textNode) => {
   return `<${tagName}>${textNode}</${tagName}>`;
 };
-
+const htmlComponent = (text, change) => {
+  return `
+  ${tagComponent("header", "헤더부분")}
+  ${tagComponent("main", `현재 ${text}(은)는 ${change}이다.`)}
+  ${tagComponent("footer", "푸터부분")}
+`;
+};
+const literal = {
+  timeAndMinute: "시간과 분",
+  minutesAndSecond: "분과 초",
+  time: "시간",
+  minute: "분",
+  second: "초",
+  even: "짝수",
+  odd: "홀수",
+};
 const container = () => {
   if (hours % 2 === 0) {
     if (minutes % 2 === 0) {
-      return `
-      ${tagComponent("header", "헤더부분")}
-      ${tagComponent("main", "분도 짝수입니다.")}
-      ${tagComponent("footer", "푸터부분")}
-    `;
+      return htmlComponent(literal.timeAndMinute, literal.even);
     }
-    return `
-    ${tagComponent("header", "헤더부분")}
-    ${tagComponent("main", "시간이 짝수입니다.")}
-    ${tagComponent("footer", "푸터부분")}
-  `;
+    return htmlComponent(literal.time, literal.even);
   } else if (hours % 2 !== 0) {
     if (minutes % 2 !== 0) {
-      return `
-      ${tagComponent("header", "헤더부분")}
-      ${tagComponent("main", "분도 홀수입니다.")}
-      ${tagComponent("footer", "푸터부분")}
-    `;
+      return htmlComponent(literal.timeAndMinute, literal.odd);
     }
-    return `
-    ${tagComponent("header", "헤더부분")}
-    ${tagComponent("main", "시간이 홀수입니다")}
-    ${tagComponent("footer", "푸터부분")}
-  `;
+    return htmlComponent(literal.time, literal.odd);
   }
 };
 /**
