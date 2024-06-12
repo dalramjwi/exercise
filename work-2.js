303e9949c556572b19ea30f9052ae386be6e60d7
dalramjwi <dalramjwi@gmail.com>
2024-06-12 12:04:57 +0900

    make function

--- work-2.js
+++ work-2.js
@@ -1,5 +1,25 @@
 const component = () => {
-  let obj = {};
-  let innerFunc = () => {};
-  return "some string";
+  let obj = {
+    name: "DSF",
+    age: 23,
+    job: "ㅇㄹ",
+  };
+  let innerFunc = () => {
+    if (obj.age > 20) {
+      return " beer";
+    } else {
+      return "drink";
+    }
+  };
+  let anotherFunc = () => {
+    if (obj.job === "ㅇㄹ") {
+      return `your job is ${obj.job}!!`;
+    } else {
+      return `your job is ${obj.job}`;
+    }
+  };
+  return `<h1>${obj.name}</h1>
+  <h2>${innerFunc()}</h2>
+  <h2>${anotherFunc()}</h2>
+  `;
 };
